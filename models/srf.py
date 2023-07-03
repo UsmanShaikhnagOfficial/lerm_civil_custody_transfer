a19847e2322bd30f77689490fb8864caf4a3d410
UsmanShaikhnagOfficial <usman.shaikhnag@esehat.org>
2023-07-03 08:02:52 +0000

    location and attachment

--- models/srf.py
+++ models/srf.py
@@ -11,3 +11,28 @@ class Srf(models.Model):
         ('4-transit', 'In Transit'),
         ('5-received', 'Received at Lab')
     ], string='State', default='1-draft')
+
+    sample_picture = fields.Binary(string="Sample Picture")
+    sample_picture_name = fields.Char(string="Attachment Name")
+
+    attachment_table = fields.One2many('srf.attachments','srf_id',string="Attachment Table")
+
+    courier_name = fields.Char(string="Courier Name")
+    courier_vehicle_no = fields.Char(string="Courier Vehicle No")
+    courier_contact_no = fields.Char(string="Courier Contact No")
+    courier_description = fields.Char(string="Courier Description")
+
+    latitude = fields.Float(string="Latitude")
+    longitute = fields.Float(string="Longitude")
+
+
+
+
+
+
+class SrfAttachment(models.Model):
+    _name = 'srf.attachments'
+
+    srf_id = fields.Many2one('lerm.civil.srf')
+    attachment = fields.Binary(string="Attachment")
+    attachment_name = fields.Char(string="Attachment Name")
